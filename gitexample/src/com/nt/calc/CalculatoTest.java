3593849996364c1f798cde4a38c7952ededd5d6d
Anupam Singh <anupamsingh278@gmail.com>
2019-04-01 23:23:43 +0530

    added two classes

---
+++ gitexample/src/com/nt/calc/CalculatoTest.java
@@ -0,0 +1,12 @@
+package com.nt.calc;
+
+public class CalculatoTest {
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		int c = calc.add(10, 5);
+		System.out.println(c);
+				
+		
+	}
+
+}
---
+++ gitexample/src/com/nt/calc/Calculator.java
@@ -0,0 +1,10 @@
+package com.nt.calc;
+
+public class Calculator {
+	public int add(int a , int b)
+	{
+		
+		return a+b;
+	}
+
+}
